a002b7eba9fc2834b05d4728d28fde65ed39aa75
omer <omerbzblt613@gmail.com>
2022-06-01 23:14:24 +0300

    İnit

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by omer faruk bozbulut on 1.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
+
